ec8170dedf403a578518eda7ad958e6822afda32
Luan Eli Oliveira <luan.oliveira@olxbr.com>
2021-05-24 21:46:35 -0300

    add script database

---
+++ ms-products/scripts/postgres/02-data.sql
@@ -0,0 +1,6 @@
+\c ms_products;
+
+INSERT INTO product (name) values ('Notebook');
+INSERT INTO product (name) values ('Teclado');
+INSERT INTO product (name) values ('Mouse');
+INSERT INTO product (name) values ('Monitor');
